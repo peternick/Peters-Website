98ca3f2849e05fb2a3478ae8a4f5864cc6c271d4
Peter Apazidis <peternick2456@gmail.com>
2020-08-31 16:25:07 -0400

    Started portfolio website

---
+++ website.js
@@ -0,0 +1,40 @@
+let navWidgets = document.querySelectorAll(".nav-item");
+let navHov = document.querySelectorAll(".hovered-nav");
+let navItemHovered = 0
+for(widget of navWidgets){
+    let hoverCover = navHov[navItemHovered]
+    widget.onmouseenter = function(){
+        hoverCover.style.height = "100%"
+    }
+    widget.onmouseleave = function(){
+        hoverCover.style.height = "0%"
+    }
+    navItemHovered++;
+}
+
+let cardEles = document.querySelectorAll('.card')
+for(cardEle of cardEles){
+    let ele = cardEle
+    ele.onmouseenter = function(){
+        // console.log(typeof(window.getComputedStyle(ele).width))
+        ele.style.outline = "solid grey"
+    }
+    ele.onmouseleave = function(){
+        // console.log(typeof(window.getComputedStyle(ele).width))
+        ele.style.outline = "none"
+    }
+}
+
+// for(cardEle of cardEles){
+//     let ele = cardEle
+//     ele.onmouseenter = function(){
+//         // console.log(typeof(window.getComputedStyle(ele).width))
+//         ele.style.width = parseInt(window.getComputedStyle(ele).width) + 30 + "px"
+//         ele.style.height = parseInt(window.getComputedStyle(ele).height) + 30 + "px"
+//     }
+//     ele.onmouseleave = function(){
+//         // console.log(typeof(window.getComputedStyle(ele).width))
+//         ele.style.width = parseInt(window.getComputedStyle(ele).width) - 30 + "px"
+//         ele.style.height = parseInt(window.getComputedStyle(ele).height) - 30 + "px"
+//     }
+// }
